4c1f78c611ad3c988a5fac5809b871f2e3b4bdfc
Javier Caballero <caballerojavier13@gmail.com>
2017-04-16 22:01:23 -0300

    Mejora de los archivos de update de código, backup y restore de la base de datos.

--- restoreDataBaseBackup.sh
+++ restoreDataBaseBackup.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-git reset HEAD --hard
-
-git pull origin master
-
 cd database
 
 date=`date +%Y%m%d`
@@ -16,13 +12,14 @@ stringToSign="PUT\n\n${contentType}\n${dateFormatted}\n${relativePath}"
 s3AccessKey="AKIAIVRA64KOGYERRF2Q"
 s3SecretKey="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"
 
+stringToSign="GET\n\n${contentType}\n${dateFormatted}\n${relativePath}"
+
 signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3SecretKey} -binary | base64`
 
-curl -X PUT -T "${fileName}" \
--H "Host: ${s3Bucket}.s3.amazonaws.com" \
--H "Date: ${dateFormatted}" \
--H "Content-Type: ${contentType}" \
--H "Authorization: AWS ${s3AccessKey}:${signature}" \
-http://${s3Bucket}.s3.amazonaws.com/Backups/GoDirector/${fileName}
+curl  -H "Host: ${s3Bucket}.s3.amazonaws.com" \
+     -H "Date: ${dateFormatted}" \
+     -H "Content-Type: ${contentType}" \
+     -H "Authorization: AWS ${s3AccessKey}:${signature}" \
+     https://${s3Bucket}.s3.amazonaws.com/Backups/GoDirector/${fileName} -o $fileName
 
 cd ..
